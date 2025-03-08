fcd46300a84d1310438784a895e679b72d8c8982
SanuviGamage5043 <sanuvigamage20842@gmail.com>
2025-03-08 10:36:32 +0530

    creating new files

---
+++ SecureConnect/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoginForm.css';
+
+function LoginForm() {
+  return (
+    <div>
+      <h1 className='logindetails'>Login Details</h1>
+    </div>
+  );
+}
+
+export default LoginForm;
---
+++ SecureConnect/src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignUpForm.css';
+
+function SignUpForm() {
+  return (
+    <div>
+      <h1 className='signupdetails'>signup details</h1>
+    </div>
+  )
+}
+
+export default SignUpForm;
