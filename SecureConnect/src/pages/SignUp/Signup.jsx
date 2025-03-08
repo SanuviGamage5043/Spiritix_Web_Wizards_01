ec60345c199ffa92edef79b7d8cd5448b70cfa72
SanuviGamage5043 <sanuvigamage20842@gmail.com>
2025-03-08 11:21:27 +0530

    creating signup items

--- SecureConnect/src/pages/SignUp/Signup.jsx
+++ SecureConnect/src/pages/SignUp/Signup.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Signup.css';
+import SignupForm from '../../components/SignupForm/SignupForm';
 
 function Signup() {
   return (
-    <div>
-      
+    <div className='signuppagecontent'>
+      <h1 className='headingsignup'>Creata an account</h1>
+      <SignupForm />
     </div>
   )
 }
