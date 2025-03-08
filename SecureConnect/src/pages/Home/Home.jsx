fe2e3930aa48463b567916dad334b2a82552827a
SanuviGamage5043 <sanuvigamage20842@gmail.com>
2025-03-08 12:17:13 +0530

    log out button creating

--- SecureConnect/src/pages/Home/Home.jsx
+++ SecureConnect/src/pages/Home/Home.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
-import './Home.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom'; 
+import "./Home.css";
 
 function Home() {
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const storedUsername = localStorage.getItem("username");
+
+//     if (storedUsername) {
+//       setUsername(storedUsername);
+//     } else {
+//         navigate('/login'); 
+//     }
+//   }, [navigate]);
+
+  const handleLogout = () => {
+    //localStorage.removeItem('username');
+    navigate('/login'); 
+  };
+
+
   return (
     <div>
-      <div className='homeheader'>
-        <h1 className='messagedetails'>Hello</h1>
-        <button className='logoutbtn'>Logout</button>
+      <div className="homeheader">
+        <h1 className="messagedetails">{username ? `Hello, ${username}!` : 'Hello!'}</h1>
+        <button className="logoutbtn" onClick={handleLogout}>
+          Logout
+        </button>
       </div>
 
-      <div className='homecontent'>
-        <h1 className='welcomemessage'>Welcome to SecureConnect</h1>
+      <div className="homecontent">
+        <h1 className="welcomemessage">Welcome to SecureConnect</h1>
       </div>
     </div>
-   
-    
   );
 }
 
