083e137647414e72c3346b1440be75a13df83dc5
ChathuniRathnayake <chathuniratnayake@gmail.com>
2025-03-08 12:18:52 +0530

    signup page editing

--- SecureConnect/src/App.jsx
+++ SecureConnect/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import Signup from './pages/SignUp/Signup';
+//import Home from './pages/Home/Home';
+import Login from './components/LoginForm/LoginForm';
+import Signup from './components/SignUpForm/SignUpForm';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
+        {/*<Route path="/" element={<Home />} />*/}
+        <Route path="/signup" element={<Signup/>} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
